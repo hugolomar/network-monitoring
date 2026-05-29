14f0b3edb66e193c7b1cf7ad85bdd67a5bb951a9
hugo <correodehugo92@gmail.com>
2026-05-29 12:45:56 +0200

    docs: document SeedWork

--- src/NetworkMonitoring.Domain/SeedWork/Entity.cs
+++ src/NetworkMonitoring.Domain/SeedWork/Entity.cs
@@ -1,9 +1,15 @@
 namespace NetworkMonitoring.Domain.SeedWork;
 
+/// <summary>
+/// Base class for all entities in the Domain-Driven Design model.
+/// </summary>
 public abstract class Entity
 {
     int? _requestedHashCode;
     int? _Id;
+    /// <summary>
+    /// Gets or sets the unique identifier for this entity.
+    /// </summary>
     public virtual int? Id
     {
         get
@@ -17,29 +23,52 @@ public abstract class Entity
     }
 
     private List<INotification> _domainEvents;
+    /// <summary>
+    /// Gets the collection of domain events associated with this entity.
+    /// </summary>
     public IReadOnlyCollection<INotification> DomainEvents => _domainEvents?.AsReadOnly();
 
+    /// <summary>
+    /// Adds a domain event to the entity.
+    /// </summary>
+    /// <param name="eventItem">The domain event to add.</param>
     public void AddDomainEvent(INotification eventItem)
     {
         _domainEvents = _domainEvents ?? new List<INotification>();
         _domainEvents.Add(eventItem);
     }
 
+    /// <summary>
+    /// Removes a domain event from the entity.
+    /// </summary>
+    /// <param name="eventItem">The domain event to remove.</param>
     public void RemoveDomainEvent(INotification eventItem)
     {
         _domainEvents?.Remove(eventItem);
     }
 
+    /// <summary>
+    /// Clears all domain events from the entity.
+    /// </summary>
     public void ClearDomainEvents()
     {
         _domainEvents?.Clear();
     }
 
+    /// <summary>
+    /// Determines whether this entity is transient (not yet persisted).
+    /// </summary>
+    /// <returns>True if transient, otherwise false.</returns>
     public bool IsTransient()
     {
         return !this.Id.HasValue;
     }
 
+    /// <summary>
+    /// Determines whether the specified object is equal to the current entity.
+    /// </summary>
+    /// <param name="obj">The object to compare with the current entity.</param>
+    /// <returns>True if the specified object is equal to the current entity; otherwise, false.</returns>
     public override bool Equals(object obj)
     {
         if (obj == null || !(obj is Entity))
@@ -59,6 +88,10 @@ public abstract class Entity
             return item.Id == this.Id;
     }
 
+    /// <summary>
+    /// Serves as the default hash function.
+    /// </summary>
+    /// <returns>A hash code for the current entity.</returns>
     public override int GetHashCode()
     {
         if (!IsTransient())
@@ -72,6 +105,12 @@ public abstract class Entity
             return base.GetHashCode();
 
     }
+    /// <summary>
+    /// Equality operator.
+    /// </summary>
+    /// <param name="left">The left entity.</param>
+    /// <param name="right">The right entity.</param>
+    /// <returns>True if equal, otherwise false.</returns>
     public static bool operator ==(Entity left, Entity right)
     {
         if (Object.Equals(left, null))
@@ -80,6 +119,12 @@ public abstract class Entity
             return left.Equals(right);
     }
 
+    /// <summary>
+    /// Inequality operator.
+    /// </summary>
+    /// <param name="left">The left entity.</param>
+    /// <param name="right">The right entity.</param>
+    /// <returns>True if not equal, otherwise false.</returns>
     public static bool operator !=(Entity left, Entity right)
     {
         return !(left == right);
--- src/NetworkMonitoring.Domain/SeedWork/IUnitOfWork.cs
+++ src/NetworkMonitoring.Domain/SeedWork/IUnitOfWork.cs
@@ -1,7 +1,21 @@
 namespace NetworkMonitoring.Domain.SeedWork;
 
+/// <summary>
+/// Defines the Unit of Work pattern interface for managing transactions.
+/// </summary>
 public interface IUnitOfWork : IDisposable
 {
+    /// <summary>
+    /// Saves all changes made in this context to the database.
+    /// </summary>
+    /// <param name="cancellationToken">A cancellation token.</param>
+    /// <returns>The number of state entries written to the database.</returns>
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Saves entities and dispatches domain events.
+    /// </summary>
+    /// <param name="cancellationToken">A cancellation token.</param>
+    /// <returns>True if the save was successful.</returns>
     Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default);
 }
